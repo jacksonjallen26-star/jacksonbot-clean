65bbe3f911a32793598d6169ca7bc02235906663
Jackson Allen <jacksonjallen26@gmail.com>
2026-03-01 18:40:09 -0500

    ss78

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 const app = express();
 
 // ===== MONGODB SETUP =====
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
