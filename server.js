b762eefbabfbe766c0d792a32532e2f2db1e7562
Jackson Allen <jacksonjallen26@gmail.com>
2026-03-01 18:43:51 -0500

    hey

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ const app = express();
 
 // ===== MONGODB SETUP =====
 mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
 })
 .then(() => console.log("MongoDB connected"))
 .catch(err => console.error("MongoDB connection error:", err));
