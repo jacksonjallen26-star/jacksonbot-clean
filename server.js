05801d8ebd74e06a9192676a307b1368e39734b4
Jackson Allen <jacksonjallen26@gmail.com>
2026-03-03 00:06:32 -0500

    wwwww

--- server.js
+++ server.js
@@ -59,9 +59,10 @@ const Chat = mongoose.model("Chat", chatSchema);
 // =====================
 app.use(cors({
   origin: [
-    "https://jacksonbot-clean.vercel.app",
-    "http://localhost:3000"
-  ],
+  "https://jacksonbot-clean.vercel.app",
+  "http://localhost:3000",
+  "http://127.0.0.1:5500"
+],
   methods: ["GET", "POST"]
 }));
 app.use(express.json());
