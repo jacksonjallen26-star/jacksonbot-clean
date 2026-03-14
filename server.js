f8a1a03ba4b09a42dc2e96699cc79a8096d3a1dd
Jackson Allen <jacksonjallen26@gmail.com>
2026-03-14 11:34:50 -0400

    here we go again with the cors

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
     "https://jacksonbot-clean.vercel.app",
-    "https://jacksonbot-dashboard.vercel.app/login",
+    "https://jacksonbot-dashboard.vercel.app",
     "http://localhost:3000",
     "http://127.0.0.1:5500"
   ],
