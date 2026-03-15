0a01819d2d6248d080a7e74ea0ddb0b8a5ecd9b4
Jackson Allen <jacksonjallen26@gmail.com>
2026-03-15 14:24:07 -0400

    lower version pc

--- server.js
+++ server.js
@@ -282,10 +282,8 @@ console.log("First chunk:", chunks[0]);
 console.log("Number of vectors created:", vectors.length);
     // Step 4: Store in Pinecone
 console.log("Vectors sample:", JSON.stringify(vectors[0]).substring(0, 200));
-// Step 4: Store in Pinecone
-const namespace = index.namespace("__default__");
 console.log("Attempting upsert with", vectors.length, "vectors");
-await namespace.upsert(vectors);
+await index.upsert(vectors);
 
 res.json({ success: true, chunksStored: chunks.length });
   } catch (err) {
