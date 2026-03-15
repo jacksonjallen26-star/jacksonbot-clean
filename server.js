8c2bbb5cfd8f2c7256bb686ec691e58b4e13a360
Jackson Allen <jacksonjallen26@gmail.com>
2026-03-15 00:08:42 -0400

    request limit

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 // ===============================
 // MIDDLEWARE
 // ===============================
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
 
 app.use(cors({
   origin: [
