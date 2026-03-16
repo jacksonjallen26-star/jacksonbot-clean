ea78a0477c3bf1e3c2d1600acaffaeb99bb244ef
Jackson Allen <jacksonjallen26@gmail.com>
2026-03-16 13:49:00 -0400

    cors

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ app.use(cors({
     "https://jacksonbot-dashboard.vercel.app",
     "https://askra.app",
     "https://www.askra.app",
+    "https://app.askra.app",
     "http://localhost:3000",
     "http://127.0.0.1:5500"
   ],
